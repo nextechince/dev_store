5f3b82fa04ee1c70fd75c0078660735386545b40
nextechince <nextechince@gmail.com>
2026-08-20 16:16:56 +0300

    Update login_screen.dart

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -95,7 +95,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   const SizedBox(height: 8),
                   Text(
-                    'Welcome back to DEVSTORE',
+                    'Welcome back to App Store',
                     style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                       color: AppColors.textMuted,
                     ),
